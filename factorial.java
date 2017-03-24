6344d3c29e1ea9aa6de04ed4c790ea3df09a6e4b
theWUisReal <alvaro14uma@gmail.com>
2017-03-24 17:00:20 +0100

    refactorizacion de metodo de salida

--- factorial.java
+++ factorial.java
@@ -8,6 +8,7 @@ return BigInteger.ONE;
 return num.multiply(fact(num.subtract(BigInteger.ONE)));
 }
 }
+<<<<<<< Updated upstream
 public static void main(String []args) {
 Scanner scanInput = new Scanner(System.in);
 System.out.print("Introduzca un numero: ");
@@ -15,11 +16,24 @@ BigInteger num = BigInteger.valueOf(scanInput.nextLong());
 if (num.compareTo(BigInteger.ZERO)<0) {
 System.out.println("El factorial no esta definido"
  + " para numeros negativos");
+=======
+public static void showFac(long num) {
+if (num<0) {
+System.out.println("El factorial no esta definido" +
+" para numeros negativos");
+>>>>>>> Stashed changes
 } else {
 BigInteger fac = fact(num);
 System.out.println("El factorial de "+num+" es "+fac);
 }
+<<<<<<< Updated upstream
 
 scanInput.close();
 }
+=======
+}
+public static void main(String []args) {
+long num = getNumber();
+showFac(num);
+>>>>>>> Stashed changes
 }
